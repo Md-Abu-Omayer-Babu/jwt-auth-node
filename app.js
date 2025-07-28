78c399b21f905a8efc81796173dd09fb22ebc9ef
Md Abu Omayer Babu <abuomayerbabu@gmail.com>
2025-07-29 03:53:06 +0600

    Set up Express middleware and routing for authentication and user profile

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import authRoutes from "./routes/auth.routes.js";
+import userProfileRoutes from "./routes/userProfile.routes.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userProfileRoutes);
+
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
     console.log("Hello from JWT Auth API!");
