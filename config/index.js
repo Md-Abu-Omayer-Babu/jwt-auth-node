b03b877b33eafff2f6ab295bafb78c51b8f7e0e7
Md Abu Omayer Babu <abuomayerbabu@gmail.com>
2025-07-29 02:28:08 +0600

    Add MongoDB connection and update environment configuration

--- config/index.js
+++ config/index.js
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const config = {
-  port: process.env.PORT || 5000,
-  mongoURI: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET,
+const config = {
+  PORT: process.env.PORT || 5000,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
+
+export default config;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import http from "http";
 import app from "./app.js";
-import { config } from "./config/index.js";
+import config from "./config/index.js";
+import connectDB from "./db/dbConnect.js";
 
-const PORT = config.port;
+const PORT = config.PORT || 5000;
 
 const startServer = async () => {
   try {
+    await connectDB();
     http.createServer(app).listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
